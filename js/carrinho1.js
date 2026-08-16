3da75c0a8887b483ecd2fecec5d5360956026e52
Luvi1328 <victordemellofarinha@gmail.com>
2026-08-16 01:01:03 -0300

    adicionei coisas para o carrinho
    adicionei uma forma de colocar mais de 1 brainrot no espaço que antes ficaria 1, quando voce muda a quantidade, agora o preço muda na pagina inicial em vez de ter que colocar no carrinho para ver o total

--- js/carrinho1.js
+++ js/carrinho1.js
@@ -1,19 +1,68 @@
 let total = 0;
+let precos = document.getElementById("precos");
 
-function adicionarCarrinho(nome, preco) {
+
+
+function aumentarQuantidade(botao) {
+    // Procura qual é o card mais proximo do botão que esta clicando e aumenta sua quantidade
+
+    const card = botao.closest(".card");
+    const quantidadeElement = card.querySelector(".quantidadeItens");
+
+
+    let quantidade = Number(quantidadeElement.textContent);
+
+    if (quantidade < 10) {
+        quantidade++;
+        quantidadeElement.textContent = quantidade;
+    }
+}
+
+function diminuirQuantidade(botao) {
+    // Procura qual é o card mais proximo do botão que esta clicando e diminui sua quantidade
+    const card = botao.closest(".card");
+    const quantidadeElement = card.querySelector(".quantidadeItens");
+
+
+    let quantidade = Number(quantidadeElement.textContent);
+
+    if (quantidade > 1) {
+        quantidade--;
+        quantidadeElement.textContent = quantidade;
+    }
+
+} function atualizarPreco(atualizarPrecos, botao) {
+    // Procura dentro do card qual a quantidade de itens e atualiza o preço total ja na tela inicial
+    const card = botao.closest(".card");
+    const quantidadeElement = card.querySelector(".quantidadeItens");
+    let quantidade = Number(quantidadeElement.textContent);
+    const precosElement = card.querySelector(".precos");
+    precosElement.innerHTML = `R$ ${(atualizarPrecos * quantidade).toFixed(2).replace(".", ",")}`;
+}
+
+function adicionarCarrinho(nome, preco, botao) {
     const carrinho = document.getElementById("itens-carrinho");
+
+    // Procura dentro do card a quantidade de itens e o preço do produto
+    const card = botao.closest(".card");
+    const quantidadeElement = card.querySelector(".quantidadeItens");
+    quantidade = Number(quantidadeElement.textContent);
+    // ----------------------------------------------------------------------
 
     const produto = document.createElement("div");
     produto.classList.add("produto-carrinho");
 
+
     produto.innerHTML = `
-        <h3>${nome}</h3>
-        <p>R$ ${preco.toFixed(2).replace(".", ",")}</p>
-    `;
+          <h3>${nome}</h3>
+        <p>R$ ${(preco * quantidade).toFixed(2).replace(".", ",")}</p>
+        <p>Quantidade: ${quantidade}</p>
+        `;
+
 
     carrinho.appendChild(produto);
 
-    total += preco;
+    total += preco * quantidade;
 
     document.getElementById("total").textContent =
         "R$ " + total.toFixed(2).replace(".", ",");
@@ -47,3 +96,6 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
 });
+
+
+
