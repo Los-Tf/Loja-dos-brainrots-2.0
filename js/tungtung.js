7b73f0d144d59b0170d5a2eb5912d37d4a073085
Luvi1328 <victordemellofarinha@gmail.com>
2026-08-16 01:25:25 -0300

    adicionei umas coisas para o carrinho
    adicionei a opção de você adicionar mais de um brainrot em um espaço, e também agora você consegue ver o preço final da quantidade de produtos que você vai colocar de uma vez no carrinho direto na pagina inicial

--- js/tungtung.js
+++ js/tungtung.js
@@ -2,11 +2,12 @@
 const carrinhoAbrir = document.getElementById('abrir-carrinho');
 const carrinhoFechar = document.getElementById('fechar-carrinho');
 const sidebar = document.getElementById('barra-lateral');
-const overlay = document.getElementById('overlay');
+
 
 
 
 carrinhoAbrir.addEventListener('click', () => {
+<<<<<<< Updated upstream
   sidebar.classList.add('open');
   overlay.classList.add('show');
 });
@@ -20,13 +21,25 @@ overlay.addEventListener('click', () => {
   sidebar.classList.remove('open');
   overlay.classList.remove('show');
 });
+=======
+    sidebar.classList.add('open');
+
+});
+
+carrinhoFechar.addEventListener('click', () => {
+    sidebar.classList.remove('open');
+
+});
+
+
+>>>>>>> Stashed changes
 
 /* COMANDO DE TROCA DE PÁGINA */
 
 const paginaAtual =
   window.location.pathname.split("/").pop();
 
-const links = document.querrySeletorAll(".navbar a");
+const links = document.querySelectorAll(".navbar a");
 
 links.forEach(link => {
   if (link.getAttribute("href") === paginaAtual) {
